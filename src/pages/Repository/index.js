064aad25996494f645172563f138c30f2e86487a
Eduardo Freitas <edusf8700@gmail.com>
2020-02-20 21:22:37 -0300

    Carregando dados da api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repo)}</h1>;
+export default class Repository extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    repo: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repo);
+
+    const [repo, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repo: repo.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repo, issues, loading } = this.state;
+
+    return <h1>Repository</h1>;
+  }
 }
